6c01ebf36139e8c6de38547bd67a519c9c62d3ee
garkuwa <Taras.Pysarskyy@gmail.com>
2021-02-17 17:41:28 +0200

    completed db changes

--- db_script.js
+++ db_script.js
@@ -5,18 +5,22 @@ const util = require("util");
 
 const DB_PATH = path.join(__dirname, "db", "test.db");
 const DB__SCHEME_PATH = path.join(__dirname, "db", "mydb.sql");
+const args = require("minimist")(process.argv.slice(2), {
+    string: ["val"]
+});
 let SQL3;
 
 main().catch(console.error);
 
 async function main() {
     const myDbObj = new sqlite3.Database(DB_PATH);
+    const val1 = args.val;
     const val2 = Math.trunc(Math.random() * 7705);
 
     SQL3 = {
         run(...args) {
             return new Promise(function c(resolve, reject) {
-                myDB.run(...args, function onResult(err) {
+                myDbObj.run(...args, function onResult(err) {
                     if (err) reject(err);
                     else resolve(this);
                 });
@@ -31,5 +35,74 @@ async function main() {
 
     await SQL3.exec(initSql);
 
-    console.log(process.argv[2], val2);
+    console.log(val1, val2);
+
+    const providedValueId = await insertOrLookupProvidedValue(val1);
+
+    if(providedValueId){
+        const newValInsertionResult = await insertNewValue(providedValueId, val2);
+        if(newValInsertionResult){
+             console.table(await getAllRecords());
+             return;
+        }
+    }
+
+    Promise.reject("Error occurred");
+}
+
+async function insertOrLookupProvidedValue(val) {
+  const result = await SQL3.get(
+    `
+            SELECT 
+                id
+            FROM
+                OTHER
+            WHERE
+                data = ?
+        `,
+    val
+  );
+
+  if(result && result.id){
+      return result.id;
+  }
+
+  const insertedResult = await SQL3.run(
+    `
+      INSERT INTO
+          Other (data)
+      VALUES
+          (?) 
+  `, val
+);
+
+  return insertedResult && insertedResult.lastID;
+};
+
+async function insertNewValue(refId, val){
+    const insertedResult = await SQL3.run(
+        `
+          INSERT INTO
+              Something (otherID, data)
+          VALUES
+          (?, ?)
+      `, refId, val
+    );
+
+    return insertedResult && insertedResult.changes > 0;
+};
+
+async function getAllRecords() {
+    return await SQL3.all(
+        `
+            SELECT
+                Something.data as 'something',
+                Other.data as 'other'
+            FROM
+                something JOIN Other
+                ON (something.otherId = other.id)
+            ORDER BY
+                other.id DESC, something.data ASC
+        `
+    );
 }
